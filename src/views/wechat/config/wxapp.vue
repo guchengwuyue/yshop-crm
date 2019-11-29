a7968f0ce73a40d5571629debb5f127266ed1f47
hupeng <guchengwuyue@163.com>
2019-11-29 18:54:56 +0800

    1.3.3新增 后台微信图文发送功能，小程序配置，增加小程序授权等,修复一些bug等

---
+++ src/views/wechat/config/wxapp.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="app-container">
+    <el-form ref="form" :model="form" :rules="rules" size="small" label-width="150px">
+      <el-form-item label="AppID" >
+        <el-input v-model="form.wxapp_appId" style="width: 370px;"/>
+      </el-form-item>
+      <el-form-item label="小程序密钥" >
+        <el-input v-model="form.wxapp_secret" style="width: 370px;" type="password" />
+      </el-form-item>
+      <el-form-item label="" >
+        <el-button  type="primary" @click="doSubmit">提交</el-button>
+      </el-form-item>
+
+    </el-form>
+
+
+  </div>
+</template>
+
+<script>
+import checkPermission from '@/utils/permission'
+import initData from '@/mixins/initData'
+import { del, add, get } from '@/api/yxSystemConfig'
+import eForm from './form'
+import picUpload from '@/components/pic-upload'
+import { Message } from 'element-ui'
+export default {
+  components: { eForm, picUpload },
+  mixins: [initData],
+  data() {
+    return {
+      delLoading: false,
+      form: {
+        wxapp_appId: '',
+        wxapp_secret: ''
+      },
+      rules: {
+      }
+    }
+  },
+  created() {
+    get().then(rese=>{
+      let newObj = {}
+      rese.content.map(function (key,value) {
+        let keyName = key.menuName;
+        newObj[keyName] = key.value
+      })
+
+      this.form = newObj
+    })
+  },
+  methods: {
+    checkPermission,
+    doSubmit() {
+      add(this.form).then(res => {
+        Message({message: '设置成功',type: 'success'})
+      }).catch(err => {
+        //this.loading = false
+        console.log(err.response.data.message)
+      })
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
