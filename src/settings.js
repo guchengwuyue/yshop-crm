b911020cd351395851d024f7874696ec888a7ab7
taozi <552920369@qq.com>
2021-01-16 17:43:27 +0800

    升级版本2.3

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   /**
    * @description 网站标题
    */
-  title: 'YSHOP-2.2',
+  title: 'YSHOP-2.3',
   /**
    * @description 是否显示 tagsView
    */
@@ -42,7 +42,7 @@ module.exports = {
   /**
    * 底部文字，支持html语法
    */
-  footerTxt: '© 2019 YSHOP <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License 2.0</a>',
+  footerTxt: '© 2019-2021 YSHOP <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">All Rights Reserved</a>',
   /**
    * 备案号
    */
